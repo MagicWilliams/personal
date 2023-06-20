3e25bc9b8a8e541381d4b4f5055859bf92868c2a
David Latimore II <david.latimore@me.com>
2023-06-20 14:03:16 -0700

    spring 23 tweaks, lets get a new job

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Image from "next/image";
-import styles from "./Header.module.scss";
+import React from 'react';
+import Image from 'next/image';
+import styles from './Header.module.scss';
 
 export default function Header(props) {
   let cursorOptions = {
@@ -16,10 +16,12 @@ export default function Header(props) {
       <div
         style={colorStyles}
         className={styles.text}
-        onClick={() => (window.location.href = "/")}
+        onClick={() => (window.location.href = '/')}
       >
         <span className={styles.title}>dlii</span>
-        <p className={styles.subtitle}>maker of apps and websites and music</p>
+        <p className={styles.subtitle}>
+          i make websites & apps when i'm not making music
+        </p>
       </div>
       <div className={styles.links}>
         <Image
@@ -27,7 +29,7 @@ export default function Header(props) {
           width={20}
           height={20}
           onClick={() =>
-            (window.location.href = "https://github.com/MagicWilliams")
+            (window.location.href = 'https://github.com/MagicWilliams')
           }
           alt="Peep my github lol"
         />
@@ -37,7 +39,7 @@ export default function Header(props) {
           height={20}
           alt="Follow me on the gram you know the vibes"
           onClick={() =>
-            (window.location.href = "https://instagram.com/magic.zip")
+            (window.location.href = 'https://instagram.com/magic.zip')
           }
         />
         <Image
@@ -46,7 +48,7 @@ export default function Header(props) {
           height={20}
           onClick={() =>
             (window.location.href =
-              "mailto:david.latimore@me.com?subject=Ayyyy")
+              'mailto:david.latimore@me.com?subject=Ayyyy')
           }
           alt="Let's talk"
         />
--- components/ProjectCarousel/ProjectCarousel.js
+++ components/ProjectCarousel/ProjectCarousel.js
@@ -1,8 +1,9 @@
-import React from "react";
-import styles from "./ProjectCarousel.module.scss";
-import Marquee from "react-fast-marquee";
-import ProjectCard from "../ProjectCard/ProjectCard";
-import Link from "next/link";
+import React, { useState } from 'react';
+import styles from './ProjectCarousel.module.scss';
+import Marquee from 'react-fast-marquee';
+import ProjectCard from '../ProjectCard/ProjectCard';
+import Link from 'next/link';
+import useWindowSize from '../../utils/useWindowSize';
 
 export default function ProjectCarousel(props) {
   const { projects, links } = props;
@@ -37,16 +38,46 @@ export function ProjectColumn(props) {
 
 export function ProjectList(props) {
   const { projects } = props;
+  const [project, setProject] = useState(null);
+  const { width } = useWindowSize();
 
   return projects ? (
     <div className={styles.ProjectList}>
-      {projects.map((project, i) => {
-        const { name } = project.fields;
-        const slug = "/project/" + name.replace(/\s+/g, "-").toLowerCase();
+      {projects.map((currProject, i) => {
+        const { name } = currProject.fields;
+        const slug = '/project/' + name.replace(/\s+/g, '-').toLowerCase();
+        console.log(width > 768 && project && project.fields.name === name);
+        const thumbStyles =
+          width > 768 && project && project.fields.name === name
+            ? {
+                display: 'block',
+                zIndex: 3,
+                position: 'absolute',
+                right: '10vw',
+                top: '250px',
+                width: '50vw',
+              }
+            : {
+                display: 'none',
+              };
 
         return (
-          <div className={styles.link} key={i}>
-            <Link href={slug}>{name}</Link>
+          <div>
+            <video
+              style={thumbStyles}
+              className="Projects--listItem-thumbnail"
+              src={project.fields.media.fields.file.url}
+              autoPlay
+              loop
+              muted
+            />
+            <div
+              className={styles.link}
+              key={i}
+              onMouseOver={() => setProject(currProject)}
+            >
+              <Link href={slug}>{name}</Link>
+            </div>
           </div>
         );
       })}
